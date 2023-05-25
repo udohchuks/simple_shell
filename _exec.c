c2d7e45bf8769b834601d3605bea30df96b222e0
Olusegun Adeleke <oadeleke123@gmail.com>
2023-05-25 16:11:14 +0000

    sort exit feature

--- _exec.c
+++ _exec.c
@@ -7,23 +7,23 @@
 */
 void _exec(char **_argum, char *av)
 {
-	char *cmd1;
+	char *cmd1, err[20];
 	pid_t idcheck;
 	char *envp[] = {"TERM=xterm-256color", NULL};
-	char err[20];
 
 	_strcpy(err, av);
+	ex_code = 0;
 	if (_argum[0][0] == '/')
 	{
 		if (access(_argum[0], X_OK) == 0)
 		{
 			idcheck = fork();
 			if (idcheck == 0)
-				execve(_argum[0], _argum, envp);
+				_execve(_argum[0], _argum, envp);
 		}
 		else
 		{
-			perror(err);
+			_perror(err);
 			return;
 		}
 	}
@@ -34,16 +34,42 @@ void _exec(char **_argum, char *av)
 			cmd1 = which(_argum[0]);
 			if (cmd1 == NULL)
 			{
-				perror(err);
+				_perror(err);
 				return;
 			}
-			free(cmd1);
 		}
 		else
 			cmd1 = _argum[0];
 		idcheck = fork();
 		if (idcheck == 0)
-			execve(cmd1, _argum, envp);
+			_execve(cmd1, _argum, envp);
+	free(cmd1);
 	}
 	wait(NULL);
 }
+
+/**
+ * _execve - runs error code and exec
+ * @c: command prompt
+ * @p: arguments to command
+ * @r: environment variable
+ */
+
+void _execve(char *c, char **p, char **r)
+{
+	ex_code = 0;
+	execve(c, p, r);
+	ex_code = 1;
+}
+
+/**
+ * _perror - printing error and checking exit code
+ * @err: error string
+ */
+
+void _perror(char *err)
+{
+	ex_code = 127;
+	perror(err);
+}
+
